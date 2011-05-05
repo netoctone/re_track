fe7e752f4f0626043254d6a6239d40b7f51d4a86
Eugen <netoctone@gmail.com>
2011-05-05 16:21:43 +0300

    new shiny empty rails 3.0.7 app, db - mysql 5.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReTrack::Application.config.secret_token = 'f49521da6453ae583157956e52e786c94e5fa1061b8b3193bd7a829578158a9d9c2c50a125a3001eb6774a7bfecd7bcae2ff814567973804974b469e7e1b66b7'
