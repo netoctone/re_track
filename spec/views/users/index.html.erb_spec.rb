ea00d0d8b8025006251f9a621c1feed3bc660c7c
Eugen <netoctone@gmail.com>
2011-05-07 00:11:15 +0300

    added User model, authorization using authlogic

---
+++ spec/views/users/index.html.erb_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "users/index.html.erb" do
+  before(:each) do
+    assign(:users, [
+      stub_model(User,
+        :first_name => "First Name",
+        :last_name => "Last Name",
+        :email => "Email",
+        :crypted_password => "Crypted Password",
+        :password_salt => "Password Salt",
+        :persistence_token => "Persistence Token"
+      ),
+      stub_model(User,
+        :first_name => "First Name",
+        :last_name => "Last Name",
+        :email => "Email",
+        :crypted_password => "Crypted Password",
+        :password_salt => "Password Salt",
+        :persistence_token => "Persistence Token"
+      )
+    ])
+  end
+
+  it "renders a list of users" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "First Name".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Last Name".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Email".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Crypted Password".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Password Salt".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Persistence Token".to_s, :count => 2
+  end
+end
---
+++ spec/views/users/new.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "users/new.html.erb" do
+  before(:each) do
+    assign(:user, stub_model(User,
+      :first_name => "MyString",
+      :last_name => "MyString",
+      :email => "MyString",
+      :crypted_password => "MyString",
+      :password_salt => "MyString",
+      :persistence_token => "MyString"
+    ).as_new_record)
+  end
+
+  it "renders new user form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form", :action => users_path, :method => "post" do
+      assert_select "input#user_first_name", :name => "user[first_name]"
+      assert_select "input#user_last_name", :name => "user[last_name]"
+      assert_select "input#user_email", :name => "user[email]"
+      assert_select "input#user_crypted_password", :name => "user[crypted_password]"
+      assert_select "input#user_password_salt", :name => "user[password_salt]"
+      assert_select "input#user_persistence_token", :name => "user[persistence_token]"
+    end
+  end
+end
