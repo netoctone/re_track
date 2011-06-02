d12e60bf3d7efea3dfe0cd9d6abf779fb1c0f272
Eugen <netoctone@gmail.com>
2011-06-02 07:54:54 +0300

    tiny change in DefectTrack model

--- app/models/defect_track.rb
+++ app/models/defect_track.rb
@@ -4,31 +4,43 @@ class DefectTrack < ActiveRecord::Base
   validates :bts_account, :formatted_id, :presence => true
 
   NameOfStatus = [
+    'Review',
+    'Review',
+    'Review',
+    'In-Progress',
     'In-Progress',
     'In-Progress',
     'Sent-RFR',
     'Committed'
   ].freeze
   PercentOfStatus = [
+    0,
+    10,
+    20,
     50,
+    60,
     80,
     90,
     100
   ].freeze
   PercentToStatus = {
-    50 => 0,
-    80 => 1,
-    90 => 2,
-    100 => 3
+    0 => 0,
+    10 => 1,
+    20 => 2,
+    50 => 3,
+    60 => 4,
+    80 => 5,
+    90 => 6,
+    100 => 7
   }.freeze
 
   class << self
     def track data
       record = self.find_or_initialize_by_bts_account_id_and_formatted_id(data[:bts_account_id], data[:formatted_id])
 
-      data[:end_date] = Time.now if data[:status] == PercentToStatus[90]
+      data[:end_date] = Time.now if data[:status].to_i == PercentToStatus[90]
       data[:start_date] = Time.now if record.start_date.nil? and
-                                      data[:status] == PercentToStatus[50]
+                                      data[:status].to_i == PercentToStatus[50]
       record.update_attributes(data)
     end
   end
