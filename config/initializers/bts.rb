9ef0ec597f0864fa9fef4f1e18fcef09480b16b1
Eugen <netoctone@gmail.com>
2011-06-02 08:00:39 +0300

    created managing of defect data flow in Bts models
    changed Bts and RallyBts models

--- config/initializers/bts.rb
+++ config/initializers/bts.rb
@@ -1,11 +1,106 @@
 class Bts
   @@bts_list = []
 
-  def self.inherited descendant
-    @@bts_list << descendant
-  end
+  class << self
+    def inherited descendant
+      @@bts_list << descendant
+    end
 
-  def self.bts_list
-    @@bts_list
+    def bts_list
+      @@bts_list
+    end
+
+    def grid_config
+      @grid_config
+    end
+
+    def configure_flow config
+      @grid_config = {}
+      @track_config = {}
+      config.each do |name, config|
+        @grid_config[name] = grid_config_elem(config)
+        if elem = track_config_elem(name, config)
+          @track_config[name] = elem
+        end
+      end
+    end
+
+    def each_field_name
+      @grid_config.each { |key, val| yield key }
+    end
+
+    def convert_value_to_track name, val
+      if @track_config[name] && convert = @track_config[name][:bts_to_track]
+        convert[val]
+      else
+        val
+      end
+    end
+
+    def convert_value_to_bts name, val
+      if @track_config[name] && convert = @track_config[name][:bts_of_track]
+        convert[val]
+      else
+        val
+      end
+    end
+
+    def params_to_track_data params
+      result = {}
+      @track_config.each do |name, config|
+        if value = params[name]
+          result[config[:to]] = value
+        end
+      end
+      result
+    end
+
+    GridConfigKeys = [:header_name, :type, :editable]
+
+    private
+    def grid_config_elem config
+      res = config.select { |key, val| GridConfigKeys.member? key }
+      if config[:type] == :combo # && config[:map]
+        res[:options] = config[:map].collect do |elem|
+          map_config = { :display => elem[:display] }
+          map_config[:value] = elem[:track_value] || elem[:display]
+          map_config
+        end
+      end
+      res
+    end
+
+    def track_config_elem name, config
+      result = nil
+      if config[:track]
+        result = { :to => name }
+      end
+      if track_to = config[:track_to]
+        result = { :to => track_to }
+      end
+      if map = config[:map]
+        result ||= { :to => name }
+        if map.kind_of? Array
+          bts_to_track = {}
+          bts_of_track = {}
+          map.each do |elem|
+            track_value = elem[:track_value] || elem[:display]
+            bts_to_track[elem[:bts_value]] = track_value
+            bts_of_track[track_value] = elem[:bts_value]
+          end
+          # bts_to_track - conversion of defect's value from bts to track
+          result[:bts_to_track] = ->(val){ bts_to_track[val] }
+          result[:bts_of_track] = ->(val){ bts_of_track[val] }
+        elsif map.kind_of? Hash
+          if bts_to_track = map[:bts_to_track]
+            result[:bts_to_track] = bts_to_track
+          end
+          if bts_of_track = map[:bts_of_track]
+            result[:bts_of_track] = bts_to_track
+          end
+        end
+      end
+      result
+    end
   end
 end
--- config/initializers/rally_bts.rb
+++ config/initializers/rally_bts.rb
@@ -2,37 +2,120 @@
 class WebAPINotAvailableError < StandardError; end
 
 class RallyBts < Bts
-  class << self
-    def find_defects username, password
-      rally = rally_rest_api(username, password)
+  configure_flow({
+    :formatted_i_d => {
+      #:track => true, # if false but :track_to or :map specified, then ignored
+      :track_to => :formatted_id, # one of confs must :track_to => :formatted_id
+      #:track_to => '',
+      :header_name => 'ID',
+      :type => :string,
+      :editable => false
+    },
+    :name => {
+      :track_to => :description,
+      :type => :string,
+      :editable => false
+    },
+    :description => {
+      :type => :text
+    },
+    :state => {
+      :type => :string,
+      :editable => false
+    },
+    :schedule_state => {
+      :type => :combo,
+      :track_to => :status,
+      #or hash of lambdas-converters
+      :map => [
+        {
+          :display => 'Defined 20%',
+          # must be unique
+          :track_value => DefectTrack::PercentToStatus[20].to_s,
+          :bts_value => 'Defined'
+        },
+        {
+          :display => 'In-Progress 50%',
+          :track_value => DefectTrack::PercentToStatus[50].to_s,
+          :bts_value => 'In-Progress'
+        },
+        {
+          :display => 'In-Progress 80%',
+          :track_value => DefectTrack::PercentToStatus[80].to_s,
+          :bts_value => 'In-Progress'
+        },
+        {
+          :display => 'Completed 90%',
+          :track_value => DefectTrack::PercentToStatus[90].to_s,
+          :bts_value => 'Completed'
+        },
+        {
+          :display => 'Accepted 100%',
+          :track_value => DefectTrack::PercentToStatus[100].to_s,
+          :bts_value => 'Accepted'
+        }
+      ]
+    },
+    :submitted_by => {
+      :type => :string,
+      :editable => false
+    }
+  })
 
-      # TODO: abort temp fetching of only one spoil safe defect
-      rally.find(:defect) { equal :formatted_i_d, 'DE7635' }
-      .collect do |query_result|
-        RallyDefect.new query_result
+  class << self
+    #
+    # returns nil if cannot authenticate with given username and password
+    # thus returns nil if account is blocked
+    #
+    def authenticable? username, password
+      self.new username, password
+    rescue Rally::NotAuthenticatedError => e
+      ::Rails.logger.info e.message
+      nil
+    end
+  end
+
+  def initialize username, password
+    @rally_api = RallyRestAPI.new :username => username,
+                                  :password => password,
+                                  :logger => ::Rails.logger
+    @username = username
+    @start_index = 0
+  rescue NoMethodError, SocketError => e
+    ::Rails.logger.info "RallyRestAPI#new raised error: #{e.class}: #{e}"
+    raise WebAPINotAvailableError, 'Rally server is not available'
+  end
+
+  def find_defects quant=2
+    quant = 100 if quant > 100
+    @start_index += quant
+    username = @username # silly unnecessary metaprogramming is THE EVIL
+    @rally_api.find(:defect,
+                    :pagesize => quant,
+                    :start => @start_index-quant) { equal :owner, username }
+    .collect do |query_result|
+      row = {}
+      RallyBts.each_field_name do |name|
+        row[name] = RallyBts.convert_value_to_track(name,
+                                                    query_result.send(name))
+      end
+      row
+    end
+  end
+
+  # TODO: ensure exception raise when update fails or return nil (and change FuncController#defect_update)
+  def update_defect params
+    defect = @rally_api.find(:defect) { equal :formatted_i_d,
+                                              params[:formatted_i_d] }.first
+    update_params = {}
+    RallyBts.each_field_name do |name|
+      val = RallyBts.convert_value_to_bts(name, params[name])
+      if val != defect.send(name)
+        update_params[name] = val
       end
     end
 
-    #
-    # returns false if cannot authenticate with given username and password
-    # thus returns false if account is blocked
-    #
-    def authenticable? username, password
-      rally_rest_api username, password
-      true
-    rescue Rally::NotAuthenticatedError => e
-      ::Rails.logger.info e.message
-      false
-    end
-
-    private
-    def rally_rest_api username, password
-      RallyRestAPI.new :username => username,
-                       :password => password,
-                       :logger => ::Rails.logger
-    rescue NoMethodError => e
-      ::Rails.logger.info "RallyRestAPI#new raised error: #{e.class}: #{e}"
-      raise WebAPINotAvailableError, 'Rally server is not available'
-    end
+    defect.update(update_params)
   end
+
 end
