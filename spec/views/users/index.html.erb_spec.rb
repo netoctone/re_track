4631a0fda74a9b60f514d3ba9b92caae40d742fb
Eugen <netoctone@gmail.com>
2011-07-30 17:38:04 +0300

    finally slayed the swap with specs
    removed useless specs
    fixed obsolete specs
    added missed specs for helpers and models (empty)

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -7,34 +7,19 @@ describe "users/index.html.erb" do
         :first_name => "First Name",
         :last_name => "Last Name",
         :email => "Email",
-        :crypted_password => "Crypted Password",
-        :password_salt => "Password Salt",
-        :persistence_token => "Persistence Token"
       ),
       stub_model(User,
         :first_name => "First Name",
         :last_name => "Last Name",
         :email => "Email",
-        :crypted_password => "Crypted Password",
-        :password_salt => "Password Salt",
-        :persistence_token => "Persistence Token"
       )
     ])
   end
 
   it "renders a list of users" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "First Name".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Last Name".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
+
+    assert_select "tr>td", :text => "First Name Last Name".to_s, :count => 2
     assert_select "tr>td", :text => "Email".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Crypted Password".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Password Salt".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Persistence Token".to_s, :count => 2
   end
 end
