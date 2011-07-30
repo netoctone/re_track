d6c7f7ad418f8132ab377a972fb20ba3efbe373d
Eugen <netoctone@gmail.com>
2011-07-30 16:36:01 +0300

    modified Users and UserSessions controllers
    removed dead code
    fixed confusing routing comments

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,40 +1,32 @@
 class UserSessionsController < ApplicationController
-  # GET /user_sessions/new
-  # GET /user_sessions/new.xml
+  # /login
   def new
     @user_session = UserSession.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @user_session }
     end
   end
 
   # POST /user_sessions
-  # POST /user_sessions.xml
   def create
     @user_session = UserSession.new(params[:user_session])
 
     respond_to do |format|
       if @user_session.save
         format.html { redirect_to(:root, :notice => 'Login successful.') }
-        format.xml  { render :xml => @user_session, :status => :created, :location => @user_session }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @user_session.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /user_sessions/1
-  # DELETE /user_sessions/1.xml
+  # /logout
   def destroy
-    @user_session = UserSession.find
-    @user_session.destroy
+    UserSession.find.destroy
 
     respond_to do |format|
       format.html { redirect_to(:users, :notice => 'Goodbye!') }
-      format.xml  { head :ok }
     end
   end
 end
